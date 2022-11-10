19f18cb147d7041907a2a1b2830f82be2bd4d2b2
krs <cristianlaczko@yahoo.com>
2022-11-11 00:15:55 +0200

    Initial commit of updated version

--- client/CMT.cpp
+++ client/CMT.cpp
@@ -206,6 +206,10 @@ int main(int argc, char * argv[])
 
 	// Init filesystem and settings
 	preinitDLL(::console);
+
+	// move all files to their mod location. Execute only once, then never again!!
+	parseOriginalDataFilesAndMoveToMods();
+
 	settings.init();
 	Settings session = settings.write["session"];
 	auto setSettingBool = [](std::string key, std::string arg) {
